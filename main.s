d32963bbc4bfd46af89f408eb5617a07bd7e8c6b
dominichann <dominic@domsmacshack.com>
2024-03-04 09:48:37 -0500

    Adjust default Makefile goal and refine build process
    The Makefile has been updated to adjust the default goal to "rom.bin". Build and clean processes are refined by removing unnecessary routines and ensuring file name consistency in the build process. This is aimed at optimizing the build workflow and improving the code quality.

--- main.s
+++ main.s
@@ -1,9 +1,22 @@
+; These three routines are provided by run6502 for simple I/O
+;   sim_putchar will print the character in the accumulator to standard output
+;   sim_getchar will populate the accumulator with one character from standard input
+;   sim_exit will terminate the simulator
+.import sim_putchar, sim_getchar, sim_exit
 
-        .org $8000
-  reset:  ldx #$41    ; Letter A
-
- .org $fffc
- .word $8000
- .word $0000
+.segment "CODE"
+.org $8000
+reset:  ldx#$20    ; Letter ' '
+next:   txa
+        jsr sim_putchar
+        inx
+        cpx #$7f
+        bne next
+        lda #$0a    ; Newline
+        jsr sim_putchar
+        jmp sim_exit
+.org $fffc
+.word $8000
+.word $0000
 
 
